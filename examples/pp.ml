f3ca1375c647931f7e494b21f2d4e268fb830fa1
Ronan <you@example.com>
2017-02-24 13:41:29 +0100

    debug pretty printing

--- examples/pp.ml
+++ examples/pp.ml
@@ -1,4 +1,5 @@
 let continue_on_error = ref false
+let use_stdout = ref false
 
 let error loc fmt =
     Printf.kfprintf (fun _ -> prerr_newline () (*; exit 1*) ) stderr fmt
@@ -9,20 +10,25 @@ let print_error (loc:Utils.loc) (msg:string) : unit =
     loc.pos_fname loc.pos_lnum (loc.pos_cnum-loc.pos_bol+1) msg;
   if not !continue_on_error then exit(1)
 
-let pretty_print c =
-  Easy_format.Pretty.to_stdout (Component.ef_component c)
+let pretty_print filename c =
+  let out =
+    if !use_stdout then stdout
+    else open_out (filename ^ ".pp")
+  in
+  Easy_format.Pretty.to_channel out (Component.ef_component c)
 
 let run_on_file filename =
   try
     let input = open_in filename in
     match Parser.parse_component filename input with
-    | Ok c -> pretty_print c
+    | Ok c -> pretty_print filename c
     | Error (lc,msg) -> print_error lc msg
   with
   | Sys_error msg -> print_error Utils.dloc msg
 
 let args = [
   ("-c"    , Arg.Set continue_on_error,   "Continue on error" );
+  ("-stdout", Arg.Set use_stdout,   "Use standard output" );
   ("-I"    , Arg.String Preprocessing.add_path, "Path for definitions files" );
 ]
 

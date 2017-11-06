9e72f7592db94854fba3caff4b5748ac3b06155a
Ronan <you@example.com>
2017-11-06 19:53:37 +0100

    take into account VALUES clause when typing an implementation

--- src/typechecker.mli
+++ src/typechecker.mli
@@ -25,9 +25,9 @@ sig
   val create: unit -> t
 end
 
-val type_expression : Global.t -> Local.t -> p_expression -> t_expression Error.t_result
-val type_predicate  : Global.t -> Local.t -> p_predicate  -> t_predicate Error.t_result
-val type_substitution  : Global.t -> Local.t -> p_substitution -> t_substitution Error.t_result
+val type_expression : Global.t -> Btype.Unif.t -> Local.t -> p_expression -> t_expression Error.t_result
+val type_predicate  : Global.t -> Btype.Unif.t -> Local.t -> p_predicate  -> t_predicate Error.t_result
+val type_substitution  : Global.t -> Btype.Unif.t -> Local.t -> p_substitution -> t_substitution Error.t_result
 
 val type_component: (string -> MachineInterface.t option) -> Global.t -> p_component -> t_component Error.t_result
 val get_interface: (string -> MachineInterface.t option) -> Syntax.p_component -> MachineInterface.t Error.t_result

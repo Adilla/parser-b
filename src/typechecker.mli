b928c41b704ffc4d3b723db2885ea57ede2f8a04
Ronan <you@example.com>
2018-03-12 21:22:52 +0100

    Improved error message in typechecker when a dependence is not found.

--- src/typechecker.mli
+++ src/typechecker.mli
@@ -5,5 +5,5 @@ val allow_out_parameters_in_precondition : bool ref
 
 type t_component = (Utils.loc,Btype.t) component
 
-val type_component: (string -> Global.t_interface option) -> Global.t -> p_component -> t_component Error.t_result
-val get_interface: (string -> Global.t_interface option) -> Syntax.p_component -> (t_component*Global.t_interface) Error.t_result
+val type_component: (Utils.loc->string -> Global.t_interface option) -> Global.t -> p_component -> t_component Error.t_result
+val get_interface: (Utils.loc ->string -> Global.t_interface option) -> Syntax.p_component -> (t_component*Global.t_interface) Error.t_result

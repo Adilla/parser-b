a1057f317dd014d75f72fe0c9d1b12673991bf45
Ronan <you@example.com>
2019-11-26 13:45:52 +0100

    fix test

--- brandom/test_random_print_parse.ml
+++ brandom/test_random_print_parse.ml
@@ -11,15 +11,10 @@ let dump_component (i:int) (c:PSyntax.component) (j:int) : unit =
   let _ = Printf.fprintf out1 "%s" (Sexp.sexp_to_string sexp1) in
   close_out out1
 
-let print_and_parse (c:PSyntax.component) : (PSyntax.component*string,string) result =
+let print_and_parse (c:PSyntax.component) : (PSyntax.component*string) =
   let str = Print.component_to_string c in
-  match Parser.parse_component_from_string str with
-  | Ok c -> Ok (c,str)
-  | Error err ->
-    begin
-      prerr_endline err.Error.err_txt;
-      Error str
-    end
+  let c = Parser.parse_component_from_string str in
+  (c,str)
 
 let nb_of_tests = 10
 
@@ -27,24 +22,23 @@ let run () =
   let st = Random.get_state () in
   for i=1 to nb_of_tests do
     let c  = Generators.gen_component st in
-    match print_and_parse c with
-    | Ok (c2,ef) ->
-      if PSyntax.equal_component c c2 then
-        print_endline "Success"
-      else
-        begin
-          dump_component i c 1;
-          dump_component i c2 2;
-          Printf.fprintf (open_out ("c_dump_"^string_of_int i)) "%s" ef;
-          print_endline "Failure"
-        end
-    | Error str ->
+    let (c2,ef) = print_and_parse c in
+    if PSyntax.equal_component c c2 then
+      print_endline "Success"
+    else
       begin
-        dump_string i str;
-        print_endline "Parse Error"
+        dump_component i c 1;
+        dump_component i c2 2;
+        Printf.fprintf (open_out ("c_dump_"^string_of_int i)) "%s" ef;
+        print_endline "Failure"
       end
   done
 
 let () =
-  let () = Random.self_init () in
-  run ()
+  try
+    Random.self_init ();
+    run ()
+  with
+    Error.Fatal ->
+    print_endline "Fatal error";
+    exit(1)
